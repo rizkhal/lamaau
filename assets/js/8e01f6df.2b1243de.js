d54cf93c248b5b40bd95cd5924cbc566671e9884
rizkhal <lamaaurizkhal@gmail.com>
2022-08-07 03:01:56 +0800

    Deploy website - based on 26915500d66c989ef5c3aa93207596c66c6af4ac

---
+++ assets/js/8e01f6df.2b1243de.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklamaau=self.webpackChunklamaau||[]).push([[5737],{7351:a=>{a.exports=JSON.parse('{"permalink":"/lamaau/blog/tags/facebook","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
